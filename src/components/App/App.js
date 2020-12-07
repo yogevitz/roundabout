808d8aa19b30dc4dea43640eb2edcec1477051b2
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-12-07 15:18:34 +0200

    Initial commit from Create Yoshi App

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import s from './App.scss';
+
+class App extends React.Component {
+  static propTypes = {
+    t: PropTypes.func,
+  };
+
+  render() {
+    const { t } = this.props;
+
+    return (
+      <div className={s.root}>
+        <h2 className={s.title} data-hook="app-title">
+          {t('app.title')}
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default withTranslation()(App);
