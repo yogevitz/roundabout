9e6422e6bad4ad2f8f03849b643f26de7b5d3d77
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-12-07 18:36:59 +0200

    add arrow component

---
+++ src/components/Arrow/Arrow.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { IconButton } from 'wix-style-react';
+import classes from './Arrow.scss';
+
+const skinPriorityMap = {
+  standard: 'secondary',
+  inverted: 'primary',
+  light: 'primary',
+  transparent: 'primary',
+  premium: 'primary',
+};
+
+const Arrow = ({
+  dataHook = '',
+  arrowSize = 'medium',
+  buttonSkin = 'standard',
+  icon,
+  className = '',
+  disabled = false,
+  ...remainingProps
+}) => (
+  <div {...remainingProps} data-hook={dataHook} className={className}>
+    <IconButton
+      className={classes.arrows}
+      skin={buttonSkin}
+      size={arrowSize}
+      disabled={disabled}
+      priority={skinPriorityMap[buttonSkin]}
+    >
+      {icon}
+    </IconButton>
+  </div>
+);
+
+export default Arrow;
