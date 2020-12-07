f94f43c2bf0e034bdb4c0a4593f664dbb035127c
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-12-07 18:37:25 +0200

    add arrows to the roundabout

--- src/components/Roundabout/Roundabout.js
+++ src/components/Roundabout/Roundabout.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Vehicle from '../Vehicle';
+import Arrow from '../Arrow';
+import ChevronLeftSmall from 'wix-ui-icons-common/ChevronLeftSmall';
+import ChevronRightSmall from 'wix-ui-icons-common/ChevronRightSmall';
 
 class Roundabout extends React.Component {
   static propTypes = {
@@ -12,9 +15,11 @@ class Roundabout extends React.Component {
 
     return (
       <div className={className}>
+        <Arrow icon={<ChevronLeftSmall />} />
         {[children].map((child, key) => (
           <Vehicle key={key}>{child}</Vehicle>
         ))}
+        <Arrow icon={<ChevronRightSmall />} />
       </div>
     );
   }
